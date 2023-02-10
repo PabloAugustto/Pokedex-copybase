2e6775bacf83aa5d7e0a2a71c4f62ac7f7395e76
PabloAugustto <pablo.augustto@hotmail.com>
2023-02-10 11:58:28 -0300

    svgs

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,74 @@
 <template>
-  <v-app></v-app>
+  <v-app>
+    <v-container>
+      <v-container>
+        <v-row>
+          <v-container>
+            <v-img
+              :src="require('../src/assets/pokedex.png')"
+              class="my-3"
+              contain
+              height="200"
+            />
+            <h1 class="text-center white--text mb-2" style="font-size: 5rem">
+              Pokedex
+            </h1>
+            <h1 class="text-center white--text mb-8">
+              Created by
+              <a class="red--text" href="https://neps.academy">Neps Academy</a>
+            </h1>
+          </v-container>
+        </v-row>
+
+        <v-text-field
+          v-model="search"
+          label="Pesquisar"
+          placeholder="Charmander"
+          solo
+        ></v-text-field>
+
+        <v-row>
+          <v-col
+            cols="6"
+            md="2"
+            v-for="pokemon in filtered_pokemons"
+            :key="pokemon.name"
+          >
+            <PokemonCard :pokemon="pokemon" @clicked="show_pokemon" />
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-container>
+
+    <PokemonInfoDialog
+      :show.sync="show_dialog"
+      :selected_pokemon="selected_pokemon"
+    />
+  </v-app>
 </template>
 
 <script>
+import axios from "axios";
+
+import PokemonCard from "./components/PokemonCard.vue";
+import PokemonInfoDialog from "./components/PokemonInfoDialog.vue";
+
 export default {
-  name: 'App',
+  name: "App",
 
-  components: {},
+  components: {
+    PokemonCard,
+    PokemonInfoDialog,
+  },
 
-  data: () => ({
-    //
-  }),
+  data() {
+    return {
+      pokemons: [],
+      search: "",
+      show_dialog: false,
+      selected_pokemon: null,
+    };
+  },
 
   mounted() {
     axios
