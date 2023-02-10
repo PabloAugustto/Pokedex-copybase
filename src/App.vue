8d24527ef7f659588e9d2fd6450fb5ff3bdd9365
PabloAugustto <pablo.augustto@hotmail.com>
2023-02-10 11:33:40 -0300

    mounted

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,40 @@ export default {
   data: () => ({
     //
   }),
+
+  mounted(){
+    axios
+      .get("https://pokeapi.co/api/v2/pokemon?limit=493")
+      .then((response) => {
+        this.pokemons = response.data.results;
+      });
+  },
+  methods: {
+    show_pokemon(id) {
+      axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`).then((response) => {
+        this.selected_pokemon = response.data;
+        this.show_dialog = !this.show_dialog;
+      });
+    },
+    get_move_level(move) {
+      for (let version of move.version_group_details) {
+        if (
+          version.version_group.name == "sword-shield" &&
+          version.move_learn_method.name == "level-up"
+        ) {
+          return version.level_learned_at;
+        }
+      }
+      return 0;
+    },
+  },
+  computed: {
+    filtered_pokemons() {
+      return this.pokemons.filter((item) => {
+        return item.name.includes(this.search);
+      });
+    },
+  },
 };
 </script>
 
